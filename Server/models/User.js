4b02b4437b3338d496ddb758c54bd99076622d36
shira-miller <sm0504123007@gmail.com>
2025-11-02 13:09:44 +0200

    remove password hashing middleware from user schema

--- Server/models/User.js
+++ Server/models/User.js
@@ -8,11 +8,4 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true }
 });
 
-// הצפנת סיסמה לפני שמירה
-userSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
 export default mongoose.model('User', userSchema);
