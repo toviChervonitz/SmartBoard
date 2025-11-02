3f51fb2d2e438052112cfff3e5e4cfecc1f3f438
LeaLiv <leahl11730@gmail.com>
2025-11-02 12:10:03 +0200

    Add post

--- Server/models/post.js
+++ Server/models/post.js
@@ -3,13 +3,12 @@ const mongoose = require("mongoose");
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  location: { type: String, required: true },
+  location: { type: String },
   contactInfo: {
-    phone: String,
-    email: String,
+    phone: { type: String },
+    email: { type: String },
   },
   likes: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("Post", postSchema);
