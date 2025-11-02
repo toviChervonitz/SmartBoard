b297517d2093060ea1c06a972323d5899b8b0bfb
LeaLiv <leahl11730@gmail.com>
2025-11-02 12:53:48 +0200

    Manage public posts

--- Server/app.js
+++ Server/app.js
@@ -1,16 +1,17 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
+const postsRouter = require('./routes/postRoutes');
 
-
-const HOST_NAME = process.env.HOST_NAME | '127.0.0.1';
-const PORT = process.env.PORT | 3000;
+const HOST_NAME = process.env.HOST_NAME || '127.0.0.1';
+const PORT = process.env.PORT || 3000;
 const app = express({ mergeParams: true });
 
+app.use(cors());
 
 app.use(express.json());
 
 
-const postsRouter = require('./routes/posts');
 app.use('/api/posts', postsRouter);
 
 require("./db/mongoConnection");
