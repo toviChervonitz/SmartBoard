167400fb5412b9d2027fe0d14e2ccabb53d557ae
toviChervonitz <tovichervonitz1234@gmail.com>
2025-11-03 10:10:36 +0200

    Refactor app.js to remove unused imports and clean up dotenv configuration

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,9 @@
 // app.js
-<<<<<<< HEAD
-import dotenv from 'dotenv';
-dotenv.config(); // חייב להיות ראשון
-import authRoutes from './routes/authRoutes.js';
-import postsRouter from './routes/postRoutes.js';
-
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-=======
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const postsRouter = require('./routes/postRoutes');
 const authRoutes = require('./routes/authRoutes');
->>>>>>> a25b0dfe3a9a85b62659c901904af4cb1eee4864
 
 const HOST_NAME = process.env.HOST_NAME || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
