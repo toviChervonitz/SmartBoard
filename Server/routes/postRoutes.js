1c76957b4d47caa19d4619aab636a20834a83379
naamaheller <naama.he788@gmail.com>
2025-11-03 13:19:38 +0200

    log out

--- Server/routes/postRoutes.js
+++ Server/routes/postRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postController");
 
-router.get("/posts/", postController.getAllPosts);
+router.get("/", postController.getAllPosts);
 router.get("/posts/:id", postController.getPostById);
 router.post("/posts/", postController.createPost);
 router.delete("/posts/:id", postController.deletePost);
