0b92f6068275611de57bdd108ef8ab982bb1ad5f
toviChervonitz <tovichervonitz1234@gmail.com>
2025-11-03 13:35:15 +0200

    Refactor post routes to unify endpoint structure and remove redundant paths

--- Server/routes/postRoutes.js
+++ Server/routes/postRoutes.js
@@ -3,18 +3,11 @@ const router = express.Router();
 const postController = require("../controllers/postController");
 
 router.get("/", postController.getAllPosts);
-<<<<<<< HEAD
 router.get("/:id", postController.getPostById);
 router.post("/", postController.createPost);
 router.delete("/:id", postController.deletePost);
 router.put("/:id", postController.updatePost);
 router.get("/user/:userId", postController.getPostsByUser);
 
-=======
-router.get("/posts/:id", postController.getPostById);
-router.post("/posts/", postController.createPost);
-router.delete("/posts/:id", postController.deletePost);
-router.put("/posts/:id", postController.updatePost);
->>>>>>> 96b79c26613ea8a616fab1c9d77d4f3c24bb31ed
 
 module.exports = router;
