00f39052de7211baa32f17b26ab8a2bd2b7758b0
LeaLiv <leahl11730@gmail.com>
2025-11-03 09:31:29 +0200

    add Navbar

--- Client/clientSmartBoard/src/App.tsx
+++ Client/clientSmartBoard/src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
 import PublicPosts from './pages/PublicPosts';
+import Navbar from './components/Navbar';
 
 function App() {
 
   return (
      <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<PublicPosts />} />
       </Routes>
---
+++ Client/clientSmartBoard/src/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import { AppBar, Toolbar, Button, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { getFromLocalStorage } from "../services/localstorage";
+
+export default function Navbar() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // נניח שבדיקה מתבצעת לפי טוקן בלוקל סטורג'
+  useEffect(() => {
+    const user = getFromLocalStorage<string>('userLogin');
+    setIsLoggedIn(!!user);
+    // setIsLoggedIn(true)
+  }, []);
+
+  return (
+    <AppBar position="fixed" color="primary" sx={{ width: '100%' }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        {/* כותרת האתר */}
+        <Typography variant="h6" component="div">
+          לוח חכם
+        </Typography>
+
+        {/* קישורים */}
+        <div>
+          <Button color="inherit" component={Link} to="/">
+            כל ההודעות
+          </Button>
+
+          {isLoggedIn && (
+            <>
+              <Button color="inherit" component={Link} to="/my-posts">
+                ההודעות שלי
+              </Button>
+              <Button color="inherit" component={Link} to="/add-post">
+                הוספת מודעה
+              </Button>
+            </>
+          )}
+        </div>
+
+        {/* התחברות/התנתקות */}
+        <div>
+          {isLoggedIn ? (
+            <Button
+              color="inherit"
+              onClick={() => {
+                localStorage.removeItem("token");
+                window.location.reload();
+              }}
+            >
+              התנתקות
+            </Button>
+          ) : (
+            <Button color="inherit" component={Link} to="/login">
+              התחברות
+            </Button>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+
+  );
+}
