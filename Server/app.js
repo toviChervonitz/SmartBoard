0bb2653752e89c9715d9daed5a9b60ec6036cad6
shira-miller <sm0504123007@gmail.com>
2025-11-03 09:57:21 +0200

    Refactor code to use CommonJS syntax and update MongoDB connection handling

--- Server/app.js
+++ Server/app.js
@@ -1,12 +1,9 @@
 // app.js
-import dotenv from 'dotenv';
-dotenv.config(); // חייב להיות ראשון
-import authRoutes from './routes/authRoutes.js';
-
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const postsRouter = require('./routes/postRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 const HOST_NAME = process.env.HOST_NAME || '127.0.0.1';
 const PORT = process.env.PORT || 3000;
@@ -20,10 +17,10 @@ app.use(express.json());
 app.use('/api/posts', postsRouter);
 app.use('/api/auth', authRoutes);
 
-require("./db/mongoConnection");
+const { connectMongo } = require('./db/mongoConnection');
 
+connectMongo();
 
 app.listen(PORT, HOST_NAME, () => {
-    console.log('server is up and running');
-
-})
+  console.log('Server is up and running');
+});
